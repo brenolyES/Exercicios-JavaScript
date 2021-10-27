b9d30c99ee968053bc7746d5398fce1534349a2c
brenolyES <brenoly.porto@upe.br>
2021-10-27 14:12:07 -0300

    estrutura para os exercicios criada

---
+++ exe1.js
@@ -0,0 +1,3 @@
+// 1) Crie uma função que inverta um número; (NÃO pode usar a função revert())
+//    Exemplo: minhaFuncao(370914) // retorno esperado: 419073;
+
---
+++ exe3.js
@@ -0,0 +1,5 @@
+// 3) Crie uma função que recebe uma string e coloca todas as primeiras letras em maiúsculo;
+// Exemplo: minhaFuncao( 'hoje faremos o trabalho em dupla, que legal - sqn' )
+// // neste caso retorna: 'Hoje Faremos O Trabalho Em Dupla, Que Legal - Sqn';
+
+
